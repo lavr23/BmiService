0e95b79be9fbe7dec7fb65c896a3aedbbf4805fe
Vladimir Lavrikov <vlavrikov83@gmail.com>
2021-12-09 19:08:09 +0300

    delete notes

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 public class BmiService {
     public double calculate (double weight, double height) {
 
-        weight = 85;
-        height = 1.85;
+
         double bmi = (weight / (height * height));
 
         return bmi;
